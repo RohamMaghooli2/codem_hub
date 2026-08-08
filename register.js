691875f8f97b849732e081dcdfcb841338cdeb11
noob _shekanan <roham.maghooli1392@gmail.com>
2026-08-08 20:27:26 +0330

    Add files via upload

--- register.js
+++ register.js
@@ -1,11 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import {
+    getFirestore,
+    doc,
+    setDoc
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDY7CJFK8tEiHxRqAoM2Dbn0DjH-Tq02vs",
+    authDomain: "codem-hub.firebaseapp.com",
+    projectId: "codem-hub",
+    storageBucket: "codem-hub.firebasestorage.app",
+    messagingSenderId: "1031768742138",
+    appId: "1:1031768742138:web:7f040ba1900de5a3ed9143"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const registerBtn = document.getElementById("registerBtn");
 
-registerBtn.addEventListener("click", function () {
+registerBtn.addEventListener("click", async function () {
+
+    const teamName = document.getElementById("teamName").value;
+    const player1 = document.getElementById("player1").value;
+    const player1Id = document.getElementById("player1Id").value;
+    const player2 = document.getElementById("player2").value;
+    const player2Id = document.getElementById("player2Id").value;
 
     const code = "CODM-" + (Math.floor(Math.random() * 9000) + 1000);
 
-    localStorage.setItem("trackingCode", code);
+    try {
 
-    window.location.href = "success.html";
+        await setDoc(doc(db, "registrations", code), {
+            teamName: teamName,
+            player1: player1,
+            player1Id: player1Id,
+            player2: player2,
+            player2Id: player2Id,
+            trackingCode: code
+        });
+
+        localStorage.setItem("trackingCode", code);
+
+        window.location.href = "success.html";
+
+    } catch (error) {
+
+        console.error("خطا در ثبت نام:", error);
+
+        alert("ثبت نام انجام نشد. دوباره تلاش کنید.");
+
+    }
 
 });
